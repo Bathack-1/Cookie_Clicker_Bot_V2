d3af6d69a663e86343375297e1448a930c210b02
Christoffer <batgamebatgame@gmail.com>
2023-04-25 20:55:01 +0200

    Endret til å kjøre egne funksjoner for å finne bildene

--- Bot/Kalibrering.py
+++ Bot/Kalibrering.py
@@ -7,7 +7,7 @@ import keyboard
 
 
 #klasse som brukes for å finen statiske elementer som hovedkjeksen og nyhetene
-class kalibrerings_verktøy:
+class KalibreringsVerktøy:
 
     """
     finne hoved kjeksen
@@ -18,27 +18,17 @@ class kalibrerings_verktøy:
     en hoved funksjon: finne_element_via_bilde()
     """
 
-    @staticmethod
-    def finne_element_via_bilde(fil_navn, skjermbilde=pyautogui.screenshot(), område=None,  sikkerhet=0.4):
+
+    def finne_senter_til_element_via_bilde(self, fil_navn, skjermbilde=pyautogui.screenshot(), sikkerhet=0.4):
         fil_bane = f"Bilder/{fil_navn}"
         element_posisjon = pyautogui.locate(fil_bane, skjermbilde, confidence=sikkerhet)
 
-        #hvis du bare trenger posisjonen til et element
-        if område is None or område == "senter":
-            element_posisjon = pyautogui.center(element_posisjon)
-            element_informasjon = {
-                "x_posisjon": int(element_posisjon[0]),
-                "y_posisjon": int(element_posisjon[1])
-            }
+        element_posisjon = pyautogui.center(element_posisjon)
+        element_informasjon = {
+            "x_posisjon": int(element_posisjon[0]),
+            "y_posisjon": int(element_posisjon[1])
+        }
 
-        #hvis du tregner posisjonen og størrelsen på et element
-        else:
-            element_informasjon = {
-                "x_posisjon": int(element_posisjon[0]),
-                "y_posisjon": int(element_posisjon[1]),
-                "bredde": int(element_posisjon[2]),
-                "høyde": int(element_posisjon[3])
-            }
         element_navn = fil_navn.split(".")[0]
         """
         "filnavn.png" => ["filnavn", "png"] => "filnavn
@@ -46,41 +36,63 @@ class kalibrerings_verktøy:
         #returnerer et dictonary med filnavnet som navn, og posisjonen som element
         return {element_navn: element_informasjon}
 
-    #den fuksjonen her brukes hvis du bruker multiprossesing
-    """
-    @staticmethod
-    def finne_element_parallelt(args):
-        metode, bilde, skjermbilde, område = args
-        return metode(bilde, skjermbilde, område)
-    """
+
+    def finne_kjøp_alle_posisjon(self, fil_navn, skjermbilde=pyautogui.screenshot(), sikkerhet=0.4):
+        fil_bane = f"Bilder/{fil_navn}"
+        element_posisjon = pyautogui.locate(fil_bane, skjermbilde, confidence=sikkerhet)
+
+        #litt lettere å bruke denne infoen når det er i en liste
+        element_informasjon = {
+            "x_posisjon": int(element_posisjon[0]),
+            "y_posisjon": int(element_posisjon[1]),
+            "bredde": int(element_posisjon[2]),
+            "høyde": int(element_posisjon[3])
+        }
+
+        #en liste for å lagre alle fargene til en rute med oppgradering
+        alle_farger = []
+
+        #Gå over alle rutene for å finne fargen dems
+        for x in range(1, 6):
+            oppgraderings_rute = (
+                element_informasjon["x_posisjon"] + (x * int(element_informasjon["bredde"] / 10)),
+                element_informasjon["y_posisjon"] + int(1.5 * element_informasjon["høyde"]) + int(element_informasjon["høyde"] / 10)
+
+            )
+            alle_farger.append(skjermbilde.getpixel(oppgraderings_rute)[2])
+
+        alle_farger.sort() #sorterer så største verdi kommer på toppen
+
+        #litt klønete måte å gjøre det på, men da får fila et hirarki
+        oppgradering_informasjon = {
+            "x_posisjon": element_informasjon["x_posisjon"] + int(element_informasjon["bredde"]/10),
+            "y_posisjon": element_informasjon["y_posisjon"] + int(1.5 * element_informasjon["høyde"]),
+            "farge": alle_farger[0], #legger til den høyeste fargen til lista
+            "kjøp_alle_knapp": element_informasjon
+        }
+        return {"oppgradere": oppgradering_informasjon}
+
 
     def kalibrer(self):
         start = time.time()
         json_tekst = {}
         skjermbilde = pyautogui.screenshot()
 
-        #alle bildene som skal brukes i kallebreringen
-        bilder = [
-            ("hoved_kjeks.png", "senter"),
-            ("nyheter.png", "senter"),
-            ("alle_oppgraderinger_knapp.png", "hele")
+        #alle funksjonene og bildene som skal brukes i kallebreringen
+        funksjoner_og_bilder = [
+            #funksjon                                   #bilde navn
+            (self.finne_senter_til_element_via_bilde, "hoved_kjeks.png"),
+            (self.finne_senter_til_element_via_bilde, "nyheter.png"),
+            (self.finne_kjøp_alle_posisjon, "alle_oppgraderinger_knapp.png")
         ]
-
-        #multiprocessing løsning
-        """""
-        with multiprocessing.Pool() as basseng:
-            resultater = basseng.map(self.finne_element_parallelt,  [(self.finne_element_via_bilde, bilde, skjermbilde, område) for bilde, område in bilder])
-
-        for resultat in resultater:
-            json_tekst.update(resultat)"""""
-
 
         #svart magi løsning (litt kjappere)
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = [executor.submit(self.finne_element_via_bilde, bilde, skjermbilde, område) for bilde, område in bilder]
+            futures = [executor.submit(funksjon, bilde, skjermbilde) for funksjon, bilde in funksjoner_og_bilder]
             for future in concurrent.futures.as_completed(futures):
                 json_tekst.update(future.result())
 
+        #siste som alltid skal gjøres, skrive posisjonene til json filen
         with open("statiske_posisjoner.json", "w") as json_fil:
             json.dump(json_tekst, json_fil, indent=4)
 
